f35f1efd5244dfe9524dbf8a823508a54f7ab12b
soul_hb <silasfilho765@gmail.com>
2026-08-30 13:57:13 -0300

    refactor: turn bot into pure Discord core

--- bot/bot.py
+++ bot/bot.py
@@ -7,7 +7,6 @@ import discord
 from discord.ext import commands
 
 from .config import Settings
-from .database import Database
 
 log = logging.getLogger(__name__)
 
@@ -22,34 +21,21 @@ class AquiJas(commands.Bot):
             help_command=None,
         )
         self.settings = settings
-        self.db = Database(settings.database_path)
-        self._commands_synced = False
 
     async def setup_hook(self) -> None:
-        await self.db.connect()
         await self.load_extension("bot.cogs.core")
         await self.load_extension("bot.cogs.v1_admin")
-
-        if self.settings.dev_guild_id:
-            guild = discord.Object(id=self.settings.dev_guild_id)
-            self.tree.copy_global_to(guild=guild)
-            await self.tree.sync(guild=guild)
-            log.info("Synced commands to DEV_GUILD_ID=%s", self.settings.dev_guild_id)
 
     async def on_ready(self) -> None:
         if self.user is None:
             return
-
-        if not self._commands_synced:
-            for guild in self.guilds:
-                self.tree.copy_global_to(guild=guild)
-                try:
-                    synced = await self.tree.sync(guild=guild)
-                    log.info("Synced %d commands to guild %s", len(synced), guild.id)
-                except discord.HTTPException:
-                    log.exception("Failed to sync commands to guild %s", guild.id)
-            self._commands_synced = True
-
+        for guild in self.guilds:
+            self.tree.copy_global_to(guild=guild)
+            try:
+                synced = await self.tree.sync(guild=guild)
+                log.info("Synced %d commands to guild %s", len(synced), guild.id)
+            except discord.HTTPException:
+                log.exception("Failed to sync commands to guild %s", guild.id)
         log.info("Logged in as %s (%s)", self.user, self.user.id)
         log.info("Connected to %d guild(s)", len(self.guilds))
 
@@ -60,10 +46,6 @@ class AquiJas(commands.Bot):
             log.info("Synced %d commands to new guild %s", len(synced), guild.id)
         except discord.HTTPException:
             log.exception("Failed to sync commands to new guild %s", guild.id)
-
-    async def close(self) -> None:
-        await self.db.close()
-        await super().close()
 
 
 def create_bot(settings: Settings) -> AquiJas:
