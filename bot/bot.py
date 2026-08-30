ee69bd71c0df6b4fd60465439f01414c98d5e099
soul_hb <silasfilho765@gmail.com>
2026-08-30 13:10:14 -0300

    fix: make slash command sync deterministic

--- bot/bot.py
+++ bot/bot.py
@@ -25,6 +25,7 @@ class AquiJas(commands.Bot):
         self.settings = settings
         self.db = Database(settings.database_path)
         self.tools = ToolRegistry()
+        self._commands_synced = False
 
     async def setup_hook(self) -> None:
         await self.db.connect()
@@ -36,15 +37,27 @@ class AquiJas(commands.Bot):
         ):
             await self.load_extension(extension)
 
+        # If DEV_GUILD_ID is set in Shard Cloud, sync there during startup.
+        # Guild commands propagate immediately and do not depend on global sync.
+        if self.settings.dev_guild_id:
+            guild = discord.Object(id=self.settings.dev_guild_id)
+            self.tree.copy_global_to(guild=guild)
+            await self.tree.sync(guild=guild)
+            log.info("Synced commands to DEV_GUILD_ID=%s", self.settings.dev_guild_id)
+
     async def on_ready(self) -> None:
         if self.user is None:
             return
-        for guild in self.guilds:
-            self.tree.copy_global_to(guild=guild)
-            try:
-                await self.tree.sync(guild=guild)
-            except discord.HTTPException:
-                log.exception("Failed to sync commands to guild %s", guild.id)
+
+        if not self._commands_synced:
+            for guild in self.guilds:
+                self.tree.copy_global_to(guild=guild)
+                try:
+                    await self.tree.sync(guild=guild)
+                except discord.HTTPException:
+                    log.exception("Failed to sync commands to guild %s", guild.id)
+            self._commands_synced = True
+
         log.info("Logged in as %s (%s)", self.user, self.user.id)
         log.info("Connected to %d guild(s)", len(self.guilds))
 
